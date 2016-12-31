3fefe96c570845b59c26cd1a61523ec7fae9fd96
Petru Mihai <pitimihai@gmail.com>
2016-12-31 21:37:14 +0200

    + Background caching

--- include/Background.hpp
+++ include/Background.hpp
@@ -10,7 +10,11 @@ public:
     static const sf::Vector2f table_position;
 
 private:
-    sf::Vector2f m_size;
+    sf::RenderTexture m_cache;
+    sf::Sprite        m_sprite;
+    sf::Vector2f      m_size;
+
+    void M_update();
 
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states)const;
 
@@ -21,11 +25,13 @@ public:
     {
         m_size.x = width;
         m_size.y = height;
+        M_update();
     }
     template<typename T> void setSize(const sf::Vector2<T> size)
     {
         m_size.x = size.x;
         m_size.y = size.y;
+        M_update();
     }
 
     sf::Vector2f getSize()const;
--- src/Background.cpp
+++ src/Background.cpp
@@ -1,4 +1,5 @@
 #include "Background.hpp"
+#include "Exception.hpp"
 
 const sf::Vector2f Background::header_position = {11.0f, 12.0f};
 const sf::Vector2f Background::table_position = {12.0f, 56.0f};
@@ -6,41 +7,52 @@ const sf::Vector2f Background::table_position = {12.0f, 56.0f};
 void Background::draw(sf::RenderTarget& target, sf::RenderStates states)const
 {
     states.transform.combine(this->getTransform());
+    target.draw(m_sprite, states);
+}
+
+Background::Background() : m_cache(), m_sprite(m_cache.getTexture()), m_size()
+{}
+
+void Background::M_update()
+{
+    if(m_cache.getSize() != sf::Vector2u(m_size.x, m_size.y))
+        if(m_cache.create(m_size.x, m_size.y) == false)
+            throw Exception(__LINE__, __FILE__);
 
     sf::RectangleShape rect;
     rect.setSize(sf::Vector2f(m_size.x, m_size.y));
     rect.setFillColor(sf::Color::White);
-    target.draw(rect, states);
+    m_cache.draw(rect);
 
     rect.setFillColor(sf::Color(192, 192, 192));
     rect.setPosition(3.0f, 4.0f);
     rect.setSize(sf::Vector2f(m_size.x - 3, m_size.y - 4));
-    target.draw(rect, states);
+    m_cache.draw(rect);
 
     rect.setFillColor(sf::Color(128, 128, 128));
 
     rect.setSize(sf::Vector2f(m_size.x - (9 + 6), 2.0f));
     rect.setPosition(9.0f, 10.0f);
-    target.draw(rect, states);
+    m_cache.draw(rect);
     rect.move(0.0f, 43.0f);
-    target.draw(rect, states);
+    m_cache.draw(rect);
     rect.setSize(sf::Vector2f(m_size.x - (9 + 6 + 1), 1.0f));
     rect.move(0.0f, 2.0f);
-    target.draw(rect, states);
+    m_cache.draw(rect);
 
     rect.setSize(sf::Vector2f(2.0f, 34.0f));
     rect.setPosition(9.0f, 12.0f);
-    target.draw(rect, states);
+    m_cache.draw(rect);
     rect.setSize(sf::Vector2f(2.0f, m_size.y - (56 + 6)));
     rect.move(0.0f, 44.0f);
-    target.draw(rect, states);
+    m_cache.draw(rect);
     rect.setSize(sf::Vector2f(1.0f, m_size.y - (56 + 6 + 1)));
     rect.move(2.0f, 0.0f);
-    target.draw(rect, states);
-}
+    m_cache.draw(rect);
 
-Background::Background() : m_size()
-{}
+    m_cache.display();
+    m_sprite.setTextureRect(sf::IntRect(0, 0, m_size.x, m_size.y));
+}
 
 sf::Vector2f Background::getSize()const
 {
